9f4c4e467d2d70819791a0422c7656c357e3cde3
caio.menezes <caio.menezes@TI-03>
2026-01-30 15:20:06 -0300

    Add project files.

---
+++ biblioteca-console-csharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+
+
+
+        }
+    }
+}
