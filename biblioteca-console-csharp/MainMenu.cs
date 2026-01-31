646853d5a7cc5731c464af181871a62cb3573196
Caio Menezes <caiomesi@gmail.com>
2026-01-30 23:36:25 -0300

    Atualização 0.0.1 do projeto de biblioteca, criação básica da classe de gestão de livros
    STATUS: Atualização incompleta, faltam métodos e classes

---
+++ biblioteca-console-csharp/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using biblioteca_console_csharp.gestao_livro;
+
+namespace MyApp
+{
+    internal class MainMenu
+    {
+        static void Main(string[] args)
+        {
+            Book newBook = new Book();
+            newBook.Registration();
+        }
+
+
+        
+
+
+    }
+}
